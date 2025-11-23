db309a571230280440540056f62e58c37a4db458
bobojonov-sobirjon <sobirbobojonov05763104@gmail.com>
2025-11-23 12:10:30 +0500

    changed

--- config/settings.py
+++ config/settings.py
@@ -216,13 +216,13 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 SITE_ID = 1
 
 # Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sobirbobojonov2000@gmail.com'
-EMAIL_HOST_PASSWORD = 'harntaefuxuvlqqw'
-DEFAULT_FROM_EMAIL = 'sobirbobojonov2000@gmail.com'
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'sobirbobojonov2000@gmail.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'harntaefuxuvlqqw')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'sobirbobojonov2000@gmail.com')
 
 
 TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
